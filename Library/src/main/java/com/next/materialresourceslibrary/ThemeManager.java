77a714d499c76f57e56b71ad4c00a49e2d8850f6
masoud <1kingdom2011@gmail.com>
2019-05-14 17:54:46 +0430

    initial release.

---
+++ Library/src/main/java/com/next/materialresourceslibrary/ThemeManager.java
@@ -0,0 +1,127 @@
+package com.peak.materialresourceslibrary;
+
+public class ThemeManager
+{
+    public static int getActivityTheme(int theme)
+    {
+        if (theme == 0)
+        {
+            return R.style.AppThemeRed_NoActionBar;
+        } else if (theme == 1)
+        {
+            return R.style.AppThemeRedDark_NoActionBar;
+        } else if (theme == 2)
+        {
+            return R.style.AppThemePink_NoActionBar;
+        } else if (theme == 3)
+        {
+            return R.style.AppThemePinkDark_NoActionBar;
+        } else if (theme == 4)
+        {
+            return R.style.AppThemePurple_NoActionBar;
+        } else if (theme == 5)
+        {
+            return R.style.AppThemePurpleDark_NoActionBar;
+        } else if (theme == 6)
+        {
+            return R.style.AppThemeDeepPurple_NoActionBar;
+        } else if (theme == 7)
+        {
+            return R.style.AppThemeDeepPurpleDark_NoActionBar;
+        } else if (theme == 8)
+        {
+            return R.style.AppThemeIndigo_NoActionBar;
+        } else if (theme == 9)
+        {
+            return R.style.AppThemeIndigoDark_NoActionBar;
+        } else if (theme == 10)
+        {
+            return R.style.AppThemeBlue_NoActionBar;
+        } else if (theme == 11)
+        {
+            return R.style.AppThemeBlueDark_NoActionBar;
+        } else if (theme == 12)
+        {
+            return R.style.AppThemeLightBlue_NoActionBar;
+        } else if (theme == 13)
+        {
+            return R.style.AppThemeLightBlueDark_NoActionBar;
+        } else if (theme == 14)
+        {
+            return R.style.AppThemeCyan_NoActionBar;
+        } else if (theme == 15)
+        {
+            return R.style.AppThemeCyanDark_NoActionBar;
+        } else if (theme == 16)
+        {
+            return R.style.AppThemeTeal_NoActionBar;
+        } else if (theme == 17)
+        {
+            return R.style.AppThemeTealDark_NoActionBar;
+        } else if (theme == 18)
+        {
+            return R.style.AppThemeGreen_NoActionBar;
+        } else if (theme == 19)
+        {
+            return R.style.AppThemeGreenDark_NoActionBar;
+        } else if (theme == 20)
+        {
+            return R.style.AppThemeLightGreen_NoActionBar;
+        } else if (theme == 21)
+        {
+            return R.style.AppThemeLightGreenDark_NoActionBar;
+        } else if (theme == 22)
+        {
+            return R.style.AppThemeLime_NoActionBar;
+        } else if (theme == 23)
+        {
+            return R.style.AppThemeLimeDark_NoActionBar;
+        } else if (theme == 24)
+        {
+            return R.style.AppThemeYellow_NoActionBar;
+        } else if (theme == 25)
+        {
+            return R.style.AppThemeYellowDark_NoActionBar;
+        } else if (theme == 26)
+        {
+            return R.style.AppThemeAmber_NoActionBar;
+        } else if (theme == 27)
+        {
+            return R.style.AppThemeAmberDark_NoActionBar;
+        } else if (theme == 28)
+        {
+            return R.style.AppThemeOrange_NoActionBar;
+        } else if (theme == 29)
+        {
+            return R.style.AppThemeOrangeDark_NoActionBar;
+        } else if (theme == 30)
+        {
+            return R.style.AppThemeDeepOrange_NoActionBar;
+        } else if (theme == 31)
+        {
+            return R.style.AppThemeDeepOrangeDark_NoActionBar;
+        } else if (theme == 32)
+        {
+            return R.style.AppThemeBrown_NoActionBar;
+        } else if (theme == 33)
+        {
+            return R.style.AppThemeBrownDark_NoActionBar;
+        } else if (theme == 34)
+        {
+            return R.style.AppThemeGrey_NoActionBar;
+        } else if (theme == 35)
+        {
+            return R.style.AppThemeGreyDark_NoActionBar;
+        } else if (theme == 36)
+        {
+            return R.style.AppThemeBlueGrey_NoActionBar;
+        } else if (theme == 37)
+        {
+            return R.style.AppThemeBlueGreyDark_NoActionBar;
+        } else if (theme == 38)
+        {
+            return R.style.AppThemeNightMode_NoActionBar;
+        }
+        return 0;
+    }
+}
---
+++ Sample/src/main/java/com/next/materialresources/MainActivity.java
@@ -0,0 +1,19 @@
+package com.next.materialresources;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		// color sources for old gradients
+		// material color from 300 to 700
+		// colors sources for new gradients
+		// https://webgradients.com
+	}
+}
